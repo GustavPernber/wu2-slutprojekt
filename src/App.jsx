55818bb2904744d96e6b3263bd0b463b850d3067
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-04-29 22:25:54 +0200

    update. visar kameror

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import CameraCard from './components/CameraCard'
 import './scss/main.css'
 import {useEffect, useState} from 'react'
 
@@ -10,25 +11,46 @@ function App() {
 
   useEffect(()=>{
     
-    async function getAllCameras(){
-
-      const url=`http://data.goteborg.se/TrafficCamera/v1.0/TrafficCameras/${key}?format=json`
-      const result= await (await fetch(url)).json()
-      console.log(result)
-      setCameras(result)
-      
-    }
-
+    
     getAllCameras()
   },[])
+  
+  async function getAllCameras(){
 
+    const url=`http://data.goteborg.se/TrafficCamera/v1.0/TrafficCameras/${key}?format=json`
+    const result= await (await fetch(url)).json()
+    setCameras(result)
+    
+  }
+  
+  
+  
   
 
   return (
     <div className="App">
-      {/* <Header></Header> */}
+      <Header></Header>
       <main>
+        <div>
+          {cameras.map((data)=>{
+            const obj={
+              id:data.Id,
+              name:data.Name.split('_')[1],
+              url:data.CameraImageUrl
+            }
 
+            return(<CameraCard key={data.Name} data={obj}></CameraCard>)
+            
+
+
+          })}
+
+          {/* <CameraCard></CameraCard>
+          <CameraCard></CameraCard>
+          <CameraCard></CameraCard>
+          <CameraCard></CameraCard> */}
+
+        </div>
       </main>
     </div>
   );
