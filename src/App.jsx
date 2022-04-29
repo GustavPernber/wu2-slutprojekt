5927240e31ed43b000d3fa8d8b7c34c08e937744
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-04-29 14:06:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import Header from './components/Header'
 import './scss/main.css'
+import {useEffect, useState} from 'react'
+
 
 function App() {
+
+  const [cameras, setCameras] = useState([])
+
+  useEffect(()=>{
+    async function getAllCameras(){
+      const url=
+      const result= await fetch
+    }
+  })
+
+  
+
   return (
     <div className="App">
       <Header></Header>
+      <main>
+
+      </main>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
 export default function Header(){
-    
+    return(
+        <header className="siteHeader">
+            <h1>GBG kameror</h1>
+
+            <div>
+                <button>Uppdatera kameror</button>
+                <p>Senast uppdaterad: 10:35</p>
+            </div>
+        </header>
+    )
 }
