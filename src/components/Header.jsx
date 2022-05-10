2f38b0506ec28157868ebf4dbdc47106f2a5fb46
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-05-10 22:23:37 +0200

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
-export default function Header(props){
+export default function Header(props) {
+	return (
+		<header className="siteHeader">
+			<div>
+				<h1>GBG Kameror</h1>
 
+				<div className="search">
+					<input
+						type="text"
+						placeholder="Vilken kamera vill du se?"
+					/>
+                    <div className="icon">
+					    <span className="material-icons">search</span>
 
-    return(
-        <header className="siteHeader">
-            <div>
+                    </div>
+				</div>
 
-                <h1>GBG Kameror</h1>
-
-                <div>
-                    <button onClick={props.updateCameras}>Uppdatera kameror</button>
-                    <p>Senast uppdaterad: {props.time}</p>
-                </div>
-            </div>
-        </header>
-    )
+				<div className="updateBtn">
+					<button onClick={props.updateCameras}>
+						Uppdatera kameror
+					</button>
+					<p>Senast uppdaterad: {props.time}</p>
+				</div>
+			</div>
+		</header>
+	);
 }
-
-
-
